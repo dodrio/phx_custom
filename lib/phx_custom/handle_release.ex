fdc683cd93d1afe820607efa5a49a53bd4031071
c4710n <c4710n@users.noreply.github.com>
2021-09-04 00:56:22 +0800

    feat: adjust for Phoenix 1.6

--- lib/phx_custom/handle_release.ex
+++ lib/phx_custom/handle_release.ex
@@ -10,21 +10,9 @@ defmodule PhxCustom.HandleRelease do
     path = Keyword.get(assigns, :path)
     template_base = Path.expand("templates/release", :code.priv_dir(@app))
 
-    [
-      "config/prod.secret.exs"
-    ]
-    |> Enum.map(&Path.expand(&1, root))
-    |> Generator.delete()
-
     Generator.copy_file(
       Path.join(template_base, "release_tasks.ex"),
       Path.join([root, path.ctx_lib, "release_tasks.ex"]),
-      assigns
-    )
-
-    Generator.copy_file(
-      Path.join(template_base, "config/runtime.exs"),
-      Path.join(root, "config/runtime.exs"),
       assigns
     )
 
